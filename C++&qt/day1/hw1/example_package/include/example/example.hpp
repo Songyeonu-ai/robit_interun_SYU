f11f1f932bad57f9da1c7afa189b61552205ef03
Songyeonu-ai <yeonu0070@kw.ac.kr>
2025-09-02 22:10:00 +0900

    Update example.hpp

--- C++&qt/day1/hw1/example_package/include/example/example.hpp
+++ C++&qt/day1/hw1/example_package/include/example/example.hpp
@@ -1,1 +1,24 @@
+#pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp
+
+
+namespace example {
+
+class Example : public rclcpp::Node{
+
+ public:
+  Example();
+private:
+ void setup();
+
+ void topicCallback(const std_msgs::msg::Int32::ConstSharedPtr& msg);
+
+private:
+ rclcpp::Subscription<std_msggs::msg::Int32>::SharedPtr subscriber_;
+ rclcpp::Publisher<std_msggs::msg::Int32>::SharedPtr publisher_;

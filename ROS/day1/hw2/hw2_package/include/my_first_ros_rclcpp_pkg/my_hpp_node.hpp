c08dc0d68a98011649c6d4b67452d148a05c39ed
Song-yeonu <yeonu0070@kw.ac.kr>
2025-09-12 07:59:04 +0900

    Add files via upload

---
+++ ROS/day1/hw2/hw2_package/include/my_first_ros_rclcpp_pkg/my_hpp_node.hpp
@@ -0,0 +1,12 @@
+// #include "rclcpp/rclcpp.hpp"
+
+// class MyCppNode : public rclcpp::Node
+// {
+// public:
+//     MyCppNode() : Node("my_cpp_node")
+//     {
+//         RCLCPP_INFO(this->get_logger(), "Hello, ROS 2 C++ Node!");
+//     }
+// };
+
+
